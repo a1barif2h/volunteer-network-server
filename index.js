1d79a8f815ac697d3d8547c6df363ebaec2341ab
a1barif2h <a1barif2h@gmail.com>
2020-10-06 02:06:27 +0600

    All done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const MongoClient = require("mongodb").MongoClient;
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const ObjectID = require("mongodb").ObjectID;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.znbod.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
@@ -21,9 +22,24 @@ client.connect((err) => {
     .db("volunteerdb")
     .collection("volunteerTask");
 
+  const registeredEvents = client.db("volunteerdb").collection("registered");
+
   app.post("/addVolunteerTask", (req, res) => {
     const newVolunteerTask = req.body;
     volunteerTaskCollection.insertMany(newVolunteerTask).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+  app.post("/add-event", (req, res) => {
+    volunteerTaskCollection.insertOne(req.body).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+  app.post("/submit-register", (req, res) => {
+    const newRegisterEvent = req.body;
+    registeredEvents.insertOne(newRegisterEvent).then((result) => {
       res.send(result.insertedCount > 0);
     });
   });
@@ -33,10 +49,28 @@ client.connect((err) => {
       res.send(documents);
     });
   });
-});
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+  app.get("/user-events", (req, res) => {
+    registeredEvents
+      .find({ email: req.headers.email })
+      .toArray((err, documents) => {
+        res.send(documents);
+      });
+  });
+
+  app.get("/registered-events", (req, res) => {
+    registeredEvents.find({}).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+
+  app.delete("/cancel-event", (req, res) => {
+    registeredEvents
+      .deleteOne({ _id: ObjectID(req.headers.id) })
+      .then((result) => {
+        res.send(result.deletedCount > 0);
+      });
+  });
 });
 
 app.listen(port);
